ee58b676a7025e2005a3c5b948f89d7e04a1a3b3
Jonathan Gilson <jonathan@gilsons.org>
2013-10-14 20:10:06 -0400

    things

--- inc/custom-bg.php
+++ inc/custom-bg.php
@@ -2,23 +2,12 @@
 function wpHTML5_register_custom_background( ) {
     $args = array(
          'default-color' => 'ffffff',
-        'default-image' => '' 
+        'default-image' => ''
     );
-    
     $args = apply_filters( 'wpHTML5_custom_background_args', $args );
-    
     if ( function_exists( 'wp_get_theme' ) ) {
         add_theme_support( 'custom-background', $args );
     } //function_exists( 'wp_get_theme' )
-    else {
-        /*
-        TODO nix else when 3.6 is released.
-        */
-        define( 'BACKGROUND_COLOR', $args[ 'default-color' ] );
-        if ( !empty( $args[ 'default-image' ] ) )
-            define( 'BACKGROUND_IMAGE', $args[ 'default-image' ] );
-        add_custom_background();
-    }
 }
 add_action( 'after_setup_theme', 'wpHTML5_register_custom_background' );
 ?>
--- inc/custom-header.php
+++ inc/custom-header.php
@@ -5,15 +5,18 @@
  *
  * You can add an optional custom header image to header.php like so ...
 
-	<?php $header_image = get_header_image();
-	if ( ! empty( $header_image ) ) { ?>
-		<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
-			<img src="<?php header_image(); ?>" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="" />
-		</a>
-	<?php } // if ( ! empty( $header_image ) ) ?>
+  <?php $header_image = get_header_image();
+  if ( ! empty( $header_image ) ) { ?>
+    <a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo
+    esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+      <img src="<?php header_image(); ?>" width="<?php echo
+      get_custom_header()->width; ?>" height="<?php echo
+      get_custom_header()->height; ?>" alt="" />
+    </a>
+  <?php } // if ( ! empty( $header_image ) ) ?>
 
  *
- * 
+ *
  * borrowed form _s starter theme.
  */
 
@@ -25,7 +28,8 @@
  * Use feature detection of wp_get_theme() which was introduced
  * in WordPress 3.4.
  *
- * @todo Rework this function to remove WordPress 3.4 support when WordPress 3.6 is released.
+ * @todo Rework this function to remove WordPress 3.4 support when WordPress
+ * 3.6 is released.
  *
  * @uses wpHTML5_header_style()
  * @uses wpHTML5_admin_style()
@@ -34,29 +38,22 @@
  * @package wordpressHTML5
  */
 function wpHTML5_custom_header_setup() {
-	$args = array(
-		'default-image'          => '',
-		'default-text-color'     => '000',
-		'width'                  => 1000,
-		'height'                 => 250,
-		'flex-height'            => true,
-		'wp-head-callback'       => 'wpHTML5_header_style',
-		'admin-head-callback'    => 'wpHTML5_admin_style',
-		'admin-preview-callback' => 'wpHTML5_admin_header_image',
-	);
+  $args = array(
+    'default-image'          => '',
+    'default-text-color'     => '000',
+    'width'                  => 1000,
+    'height'                 => 250,
+    'flex-height'            => true,
+    'wp-head-callback'       => 'wpHTML5_header_style',
+    'admin-head-callback'    => 'wpHTML5_admin_style',
+    'admin-preview-callback' => 'wpHTML5_admin_header_image',
+  );
 
-	$args = apply_filters( 'wpHTML5_custom_header_args', $args );
+  $args = apply_filters( 'wpHTML5_custom_header_args', $args );
 
-	if ( function_exists( 'wp_get_theme' ) ) {
-		add_theme_support( 'custom-header', $args );
-	} else {
-		// Compat: Versions of WordPress prior to 3.4.
-		define( 'HEADER_TEXTCOLOR',    $args['default-text-color'] );
-		define( 'HEADER_IMAGE',        $args['default-image'] );
-		define( 'HEADER_IMAGE_WIDTH',  $args['width'] );
-		define( 'HEADER_IMAGE_HEIGHT', $args['height'] );
-		add_custom_image_header( $args['wp-head-callback'], $args['admin-head-callback'], $args['admin-preview-callback'] );
-	}
+  if ( function_exists( 'wp_get_theme' ) ) {
+    add_theme_support( 'custom-header', $args );
+  }
 }
 add_action( 'after_setup_theme', 'wpHTML5_custom_header_setup' );
 
@@ -69,21 +66,22 @@ add_action( 'after_setup_theme', 'wpHTML5_custom_header_setup' );
  * of this function.
  *
  * @todo Remove this function when WordPress 3.6 is released.
- * @return stdClass All properties represent attributes of the curent header image.
+ * @return stdClass All properties represent attributes of the curent header
+ * image.
  *
  * @package wordpressHTML5
  * @since wordpressHTML5 1.1
  */
 
 if ( ! function_exists( 'get_custom_header' ) ) {
-	function get_custom_header() {
-		return (object) array(
-			'url'           => get_header_image(),
-			'thumbnail_url' => get_header_image(),
-			'width'         => HEADER_IMAGE_WIDTH,
-			'height'        => HEADER_IMAGE_HEIGHT,
-		);
-	}
+  function get_custom_header() {
+    return (object) array(
+      'url'           => get_header_image(),
+      'thumbnail_url' => get_header_image(),
+      'width'         => HEADER_IMAGE_WIDTH,
+      'height'        => HEADER_IMAGE_HEIGHT,
+    );
+  }
 }
 
 if ( ! function_exists( 'wpHTML5_header_style' ) ) :
@@ -96,34 +94,35 @@ if ( ! function_exists( 'wpHTML5_header_style' ) ) :
  */
 function wpHTML5_header_style() {
 
-	// If no custom options for text are set, let's bail
-	// get_header_textcolor() options: HEADER_TEXTCOLOR is default, hide text (returns 'blank') or any hex value
-	if ( HEADER_TEXTCOLOR == get_header_textcolor() )
-		return;
-	// If we get this far, we have custom styles. Let's do this.
-	?>
-	<style type="text/css">
-	<?php
-		// Has the text been hidden?
-		if ( 'blank' == get_header_textcolor() ) :
-	?>
-		.blogTitle,
-		.tagLine {
-			position: absolute !important;
-			clip: rect(1px 1px 1px 1px); /* IE6, IE7 */
-			clip: rect(1px, 1px, 1px, 1px);
-		}
-	<?php
-		// If the user has set a custom color for the text use that
-		else :
-	?>
-		.blogTitle a,
-		.tagLine {
-			color: #<?php echo get_header_textcolor(); ?>;
-		}
-	<?php endif; ?>
-	</style>
-	<?php
+  // If no custom options for text are set, let's bail
+  // get_header_textcolor() options: HEADER_TEXTCOLOR is default, hide text
+  // (returns 'blank') or any hex value
+  if ( HEADER_TEXTCOLOR == get_header_textcolor() )
+    return;
+  // If we get this far, we have custom styles. Let's do this.
+  ?>
+  <style type="text/css">
+  <?php
+    // Has the text been hidden?
+    if ( 'blank' == get_header_textcolor() ) :
+  ?>
+    .blogTitle,
+    .tagLine {
+      position: absolute !important;
+      clip: rect(1px 1px 1px 1px); /* IE6, IE7 */
+      clip: rect(1px, 1px, 1px, 1px);
+    }
+  <?php
+    // If the user has set a custom color for the text use that
+    else :
+  ?>
+    .blogTitle a,
+    .tagLine {
+      color: #<?php echo get_header_textcolor(); ?>;
+    }
+  <?php endif; ?>
+  </style>
+  <?php
 }
 endif; // wpHTML5_header_style
 
@@ -137,22 +136,22 @@ if ( ! function_exists( 'wpHTML5_admin_style' ) ) :
  */
 function wpHTML5_admin_style() {
 ?>
-	<style type="text/css">
-	.appearance_page_custom-header #headimg {
-		border: none;
-	}
-	#headimg h1,
-	#desc {
-	}
-	#headimg h1 {
-	}
-	#headimg h1 a {
-	}
-	#desc {
-	}
-	#headimg img {
-	}
-	</style>
+  <style type="text/css">
+  .appearance_page_custom-header #headimg {
+    border: none;
+  }
+  #headimg h1,
+  #desc {
+  }
+  #headimg h1 {
+  }
+  #headimg h1 a {
+  }
+  #desc {
+  }
+  #headimg img {
+  }
+  </style>
 <?php
 }
 endif; // wpHTML5_admin_style
@@ -166,19 +165,21 @@ if ( ! function_exists( 'wpHTML5_admin_header_image' ) ) :
  * @since wordpressHTML5 1.0
  */
 function wpHTML5_admin_header_image() { ?>
-	<div id="headimg">
-		<?php
-		if ( 'blank' == get_header_textcolor() || '' == get_header_textcolor() )
-			$style = ' style="display:none;"';
-		else
-			$style = ' style="color:#' . get_header_textcolor() . ';"';
-		?>
-		<h1><a id="name"<?php echo $style; ?> onclick="return false;" href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a></h1>
-		<div id="desc"<?php echo $style; ?>><?php bloginfo( 'description' ); ?></div>
-		<?php $header_image = get_header_image();
-		if ( ! empty( $header_image ) ) : ?>
-			<img src="<?php echo esc_url( $header_image ); ?>" alt="" />
-		<?php endif; ?>
-	</div>
+  <div id="headimg">
+    <?php
+    if ( 'blank' == get_header_textcolor() || '' == get_header_textcolor() )
+      $style = ' style="display:none;"';
+    else
+      $style = ' style="color:#' . get_header_textcolor() . ';"';
+    ?>
+    <h1><a id="name"<?php echo $style; ?> onclick="return false;" href="<?php
+    echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a></h1>
+    <div id="desc"<?php echo $style; ?>><?php bloginfo( 'description' );
+    ?></div>
+    <?php $header_image = get_header_image();
+    if ( ! empty( $header_image ) ) : ?>
+      <img src="<?php echo esc_url( $header_image ); ?>" alt="" />
+    <?php endif; ?>
+  </div>
 <?php }
 endif; // wpHTML5_admin_header_image
